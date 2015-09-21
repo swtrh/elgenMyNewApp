3f8e38c716f7d5b3e6608a0d6d75e9152a7790b6
unknown <konbe@LT-747835.statoil.net>
2015-09-21 11:38:58 +0200

    updated

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express=require('express');
 var app=express();
+var port=(process.env.PORT ||'3000');
 app.get('/', function(req,res){
 	res.send('Hello world');
 });
-app.listen(3000);
+app.listen(port);
 console.log("started at 3000");
