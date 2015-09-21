8e7f73f390736b0c686c91c7b26f8bb440a6fd0d
unknown <konbe@LT-747835.statoil.net>
2015-09-21 13:39:46 +0200

    added store.jade

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Min flotte app' });
 });
 
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+router.get('/store', function(req, res) {   
+   res.render('store', { title: 'Welcome to the store' }); 
+ 
+})
+
 module.exports = router;
